17559eb550f3f297714adf5207907ef8503197e8
Luke Hewitt <insperatum@gmail.com>
2013-04-21 12:48:25 +0100

    Node.lineNumber and node.charNumber now get set

--- src/uk/ac/ucl/comp2010/bestgroup/AST/Node.java
+++ src/uk/ac/ucl/comp2010/bestgroup/AST/Node.java
@@ -3,4 +3,9 @@ package uk.ac.ucl.comp2010.bestgroup.AST;
 public abstract class Node {
 	public int lineNumber;
 	public int charNumber;
+	
+	public void setPosition(int l, int c) {
+		lineNumber = l+1;
+		charNumber = c+1;
+	}
 }
